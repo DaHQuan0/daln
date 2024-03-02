889c4396cb0ee2b200b07e17bd613de2b260f0e6
Quan <21013237@st.phenikaa-uni.edu.vn>
2024-03-02 11:05:14 +0700

    update_firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:daln/view/home/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // Step 0: create a project on https://console.firebase.google.com and performs setting following the guidelines
+  // Step 1: flutter pud add firebase_core and firebase_auth
+  // Step 2 add below two lines:
+  // Step 3: go to sigup_screen.dart
+  WidgetsFlutterBinding
+      .ensureInitialized(); // make sure firebase is initialized before we run our application
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); // to can use firebase Product and this is a asynchronous function
+  // run again to make sure out settings working properly or not, if there is no error it means
+  // firebase initialization was proper
   runApp(const MyApp());
 }
 
